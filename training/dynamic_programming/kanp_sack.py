b9e457fabfe3aa9feec8ea928c8edbc6f530ad42
vijay.maddukuri@virtustream.com <Vijay.Maddukuri@virtustream.com>
2018-10-11 11:29:38 +0530

    Adding dynamic programming code

--- training/dynamic_programming/kanp_sack.py
+++ training/dynamic_programming/kanp_sack.py
@@ -1,16 +1,15 @@
-def knapSack(W, wt, val, n):
-    K = [[0 for x in range(W + 1)] for x in range(n + 1)]
-    # Build table K[][] in bottom up manner
-    for i in range(n + 1):
-        for w in range(W + 1):
-            if i == 0 or w == 0:
-                K[i][w] = 0
-            elif wt[i - 1] <= w:
-                K[i][w] = max(val[i - 1] + K[i - 1][w - wt[i - 1]], K[i - 1][w])
-            else:
-                K[i][w] = K[i - 1][w]
+def knapSack(cap, wt, val, n):
+    k = [[0 for i in range(cap+1)] for i in range(n+1)]
 
-    return K[n][W]
+    for i in range(n+1):
+        for j in range(cap+1):
+            if i == 0 or j == 0:
+                k[i][j] = 0
+            elif wt[i-1] <= j:
+                k[i][j] = max(val[i-1]+k[i-1][j-wt[i-1]], k[i-1][j])
+            else:
+                k[i][j] = k[i-1][j]
+    return k[n][cap]
 
 val = [60, 100, 120]
 wt = [10, 20, 30]
--- training/temp.py
+++ training/temp.py
@@ -1,11 +1,2 @@
-def sort(lst):
-    for i in range(len(lst)-1,0,-1):
-        for j in range(i):
-            temp=lst[j]
-            lst[j]=lst[j+1]
-            lst[j + 1]=temp
-    return lst
-
-newList=sort(['is2', 'Thi1s', 'T4est', '3a'])
-print newList
-
+def kanpsack(val, wt, cap, n):
+    T = [[0 for i in range()]]
