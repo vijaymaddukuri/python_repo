eecbd7c20415a7ca3e2ebde0dc9e3c84050c301f
vijay.maddukuri@virtustream.com <Vijay.Maddukuri@virtustream.com>
2019-03-13 14:57:27 +0530

    DS

--- training/demo.py
+++
@@ -1,9 +0,0 @@
-# Read and save an integer, double, and String to your variables.
-a = int(input())
-b = float(input())
-c = str(input())
-
-# Concatenate and print the String variables on a new line
-print(i+a)
-print(d+b)
-print(s+c)
